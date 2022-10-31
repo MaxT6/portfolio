2ce43191e0aaa9546f5a04e5210d40c7b90ad105
MaxT6 <maximiliant6@gmail.com>
2022-10-31 12:53:19 -0400

    Created Navbar component and stylesheets for Home and Navbar components

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import "./NavbarStyle.css"
+
+const Navbar = () => {
+  return(
+    <div className="navbar">
+      <h2>About</h2>
+      <h2>Projects</h2>
+      <h2>Games</h2>
+    </div>
+  )
+}
+
+export default Navbar
