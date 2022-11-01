57ad3ec40cf2d01c3108d3c5333f7c06bdc76317
MaxT6 <maximiliant6@gmail.com>
2022-11-01 14:19:24 -0400

    Moved images folder to Public folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import "./styles/App.css"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-const backgroundImage = new URL("./images/portfolio-background.jpg", import.meta.url)
+
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
           <Navbar />
           <div className="content">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" exact element={<Home />} />
               <Route path="/games" element={<Games />} />
             </Routes>
           </div>
--- src/components/Games.js
+++ src/components/Games.js
@@ -1,9 +1,18 @@
 import "../styles/Games.css"
+// import { Link } from "react-router-dom"
+
 
 const Games = () => {
   return (
     <div className="games">
       <h1>Images of compeleted games to go here</h1>
+      <div className="game-images-container">
+        <div className="game-images">
+          {/* <a href="https://github.com/MaxT6/roshambo" target="_blank" rel="noreferrer">
+            <img src={roshamboImage} alt="Roshambo game built with Vue.js showing the three choices: rock, paper, scissors"/>
+          </a> */}
+        </div>
+      </div>
     </div>
   );
 }
